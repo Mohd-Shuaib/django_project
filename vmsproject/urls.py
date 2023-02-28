89ce696aabef80a74206eca223cbc1c5ba444ba0
Mohd Shuaib <mohdshuaib409@gmail.com>
2023-02-28 21:42:21 +0530

    Static folder copy to settings.py

--- vmsproject/urls.py
+++ vmsproject/urls.py
@@ -1,23 +1,23 @@
 from django.contrib import admin
 from django.urls import path
-# from django.conf.urls import url
+from django.conf.urls import url
 from . import views
 
-# urlpatterns = [
-#     url(r'^$', views.index, name='home'),
-#     url(r'^$',  views.about, name="about"),
-#     url(r'^$',  views.contact, name="contact"),
-#     url(r'^$',  views.service, name="services"),
-#     url(r'^$',  views.products, name="services"),
-#     url(r'^$',  views.thank, name="thank"),
-# ]
-
 urlpatterns = [
-    path("", views.index, name="home"),
-    path("about", views.about, name="about"),
-    path("contact", views.contact, name="contact"),
-    path("service", views.service, name="services"),
-    path("products", views.products, name="services"),
-    path("thanks", views.thank, name="thank"),
-  
+    url(r'^$', views.index, name='home'),
+    url(r'^$',  views.about, name="about"),
+    url(r'^$',  views.contact, name="contact"),
+    url(r'^$',  views.service, name="services"),
+    url(r'^$',  views.products, name="services"),
+    url(r'^$',  views.thank, name="thank"),
 ]
+
+# urlpatterns = [
+#     path("", views.index, name="home"),
+#     path("about", views.about, name="about"),
+#     path("contact", views.contact, name="contact"),
+#     path("service", views.service, name="services"),
+#     path("products", views.products, name="services"),
+#     path("thanks", views.thank, name="thank"),
+  
+# ]
